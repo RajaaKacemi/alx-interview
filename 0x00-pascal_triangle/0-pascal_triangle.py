9e8f4db9b61d177451e853051854d6c3844ca4aa
rajaaKacemi <rajaa01.kacemi@gmail.com>
2024-08-06 01:34:05 -0700

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,11 @@
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the nth row.
+    Returns an empty list if n <= 0.
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # Start with the first row
 
     for i in range(1, n):
